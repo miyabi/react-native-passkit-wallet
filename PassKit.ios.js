866e06393602ea600091c9933e03e9e4dc2bc24e
Robert Marshall <lucas.marshall@gmail.com>
2019-05-18 23:49:21 -0700

    Add containsPass method

--- PassKit.ios.js
+++ PassKit.ios.js
@@ -1,36 +1,44 @@
 /**
  * @flow
  */
-'use strict'
+"use strict";
 
-import { NativeModules, NativeEventEmitter } from 'react-native'
-import type EmitterSubscription from 'EmitterSubscription'
+import { NativeModules, NativeEventEmitter } from "react-native";
+import type EmitterSubscription from "EmitterSubscription";
 
-const nativeModule = NativeModules.RNPassKit
-const nativeEventEmitter = new NativeEventEmitter(nativeModule)
+const nativeModule = NativeModules.RNPassKit;
+const nativeEventEmitter = new NativeEventEmitter(nativeModule);
 
 export default {
   ...nativeModule,
 
   addPass: (base64Encoded: string, fileProvider?: string): Promise<void> => {
-    return nativeModule.addPass(base64Encoded)
+    return nativeModule.addPass(base64Encoded);
   },
 
   presentAddPassesViewController: (base64Encoded: string): Promise<void> => {
     // eslint-disable-next-line no-console
-    console.warn('PassKit.presentAddPassesViewController is deprecated. Use PassKit.addPass instead.')
-    return nativeModule.addPass(base64Encoded)
+    console.warn(
+      "PassKit.presentAddPassesViewController is deprecated. Use PassKit.addPass instead."
+    );
+    return nativeModule.addPass(base64Encoded);
+  },
+
+  containsPass: (
+    identifier: string,
+    serialNumber: string
+  ): Promise<boolean> => {
+    return nativeModule.containsPass(identifier, serialNumber);
   },
 
   addEventListener: (
     eventType: string,
     listener: Function,
-    context: ?Object,
-  ): ?EmitterSubscription => (
-    nativeEventEmitter.addListener(eventType, listener, context)
-  ),
+    context: ?Object
+  ): ?EmitterSubscription =>
+    nativeEventEmitter.addListener(eventType, listener, context),
 
   removeEventListener: (eventType: string, listener: Function): void => {
-    nativeEventEmitter.removeListener(eventType, listener)
-  },
-}
+    nativeEventEmitter.removeListener(eventType, listener);
+  }
+};
